12ff55160fd46e143251b8655714755c040f4f33
christianivh <christianivh@hotmail.com>
2019-11-26 20:47:51 -0500

    s3 manager

---
+++ utils/s3_manager.py
@@ -0,0 +1,68 @@
+import boto3
+import botocore
+import gzip
+from io import BytesIO
+
+
+class S3Manager:
+    def __init__(self):
+        self.s3 = boto3.client('s3')
+
+    def download_file(self, bucket, key_object, local_filename, decompress=False):
+        """
+        Download object in a bucket
+        :param bucket: bucket name, without s3://
+        :param key_object: folder's and the file name
+        :param local_filename: destine local filename
+        :param decompress: True, file will be decompressed
+        :return:
+        """
+        try:
+            if decompress:
+                data = self.s3.Object(bucket, key_object)
+                n = data.get()['Body'].read()
+                gzip_file = BytesIO(n)
+                gzip_file = gzip.GzipFile(fileobj=gzip_file)
+                with open(local_filename, 'wb') as outfile:
+                    outfile.write(gzip_file.read())
+
+            with open(local_filename, 'wb') as data:
+                self.s3.download_fileobj(bucket, key_object, data)
+
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                print("Some params are not correct")
+                raise
+            return False
+
+        return True
+
+    def upload_file(self, local_filename, bucket, key_object, compress=False):
+        """
+        Upload object in a bucket
+        :param bucket: bucket name, without s3://
+        :param key_object: folder's and the file name
+        :param local_filename: source local complete
+        :param compress: True, file will be compressed
+        :return:
+        """
+        try:
+
+            with open(local_filename, 'rb') as data:
+                if compress:
+                    data = gzip.GzipFile(fileobj=data, mode='rb')
+                    key_object = key_object + '.gz'
+
+                self.s3.upload_fileobj(data, bucket, key_object)
+
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                print("Some params are not correct")
+                raise
+            return False
+
+        return True
